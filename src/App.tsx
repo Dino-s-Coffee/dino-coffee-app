f5a5d97be34be69569b31b23571aa950ca7c024d
Lam Tran <minhlam2102002@gmail.com>
2022-11-27 20:45:17 +0700

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
   setupIonicReact
 } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { homeOutline, heartOutline, cartOutline, bagOutline, personOutline } from 'ionicons/icons';
 
-import Home from './pages/Home';
-import Favorite from './pages/Favorite';
-import Tab3 from './pages/Tab3';
-import Cart from './pages/Cart';
-import Profile from './pages/Profile';
+import { IonReactRouter } from '@ionic/react-router';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -36,52 +23,17 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Navbar from './components/Navbar';
 
-setupIonicReact();
+setupIonicReact({
+  // rippleEffect: false,
+  // mode: 'md',
+});
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/favorite">
-            <Favorite />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-        </IonRouterOutlet>
-
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="home" href="/home">
-            <IonIcon icon={homeOutline} />
-          </IonTabButton>
-          <IonTabButton tab="favorite" href="/favorite">
-            <IonIcon icon={heartOutline} />
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={bagOutline} />
-          </IonTabButton>
-          <IonTabButton tab="cart" href="/cart">
-            <IonIcon icon={cartOutline} />
-          </IonTabButton>
-          <IonTabButton tab="profile" href="/profile">
-            <IonIcon icon={personOutline} />
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
+      <Navbar />
     </IonReactRouter>
   </IonApp>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -6,14 +6,14 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
+        <IonToolbar className='header-toolbar' style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+          <IonSearchbar animated={true} mode={'ios'}></IonSearchbar>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
+            <IonTitle size='large'>Home</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Home page" />
